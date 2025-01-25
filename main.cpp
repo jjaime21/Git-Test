f8b4bf950bedec2267da5cc14e5ecaa3517fd87d
Jose Jaime <jjaime2@ut.utm.edu>
2025-01-24 19:24:07 -0600

    Added sum function

--- main.cpp
+++ main.cpp
@@ -12,8 +12,41 @@
 
 using namespace std;
 
+
+/**
+ * Calulates the sum of numbers from 1 to n.
+ *
+ * @param int n Number the user enters.
+ * @pre Number the user enters.
+ * @return int Returns the sum of numbers from 1 to n.
+ * @post Returns the sum of numbers from 1 to n.
+ * 
+ */
+int sum(int n);
+
+
 int main() {
   cout << "Hello World!" << endl;
+
+  cout << "Please enter a number: ";
+  int n;
+  cin >> n;
+
+  // Call the sum function and print the result
+  if (n > 0) {
+    int result = sum(n);
+    cout << "The sum of numbers from 1 to " << n << " is: " << result << endl;
+  } else {
+    cout << "Please enter a positive number." << endl;
+  }
   
   return 0;
+}
+
+int sum(int n) {
+  int total = 0;
+  for (int i = 1; i <= n; ++i) {
+    total += i;
+  }
+  return total;
 }
