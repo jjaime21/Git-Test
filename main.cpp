3585d194ce8087062de11a123c56f968c1b85b34
Jose Jaime <jjaime2@ut.utm.edu>
2025-01-24 18:24:49 -0600

    Adding the first file of the project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Jose Jaime
+ * @date 2025-01-24
+ * @brief Hello World program
+ * 
+ * This program outputs Hello World! to the screen.
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
